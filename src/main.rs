da252f6cb258cdfdaf388319ef307ed739d0d452
Hasan <mr.k779@outlook.com>
2023-08-29 22:19:29 +0600

    Added file reading

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ mod token;
 
 use lex::Lexer;
 use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
 use token::TokenType;
 
 fn main() {
@@ -11,18 +14,34 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     assert_eq!(args.len(), 2, "Expected the teeny file");
 
-    let source = "IF+-123 foo*THEN/";
-    let mut lexer = Lexer::new(source);
+    let path_arg = get_nth_arg(1);
+    let path = Path::new(path_arg.as_str());
+    let display = path.display();
+
+    // Open the path in read-only mode, returns `io::Result<File>`
+    let mut file = match File::open(&path) {
+        Err(why) => panic!("couldn't open {}: {}", display, why),
+        Ok(file) => file,
+    };
+
+    // Read the file contents into a string, returns `io::Result<usize>`
+    let mut source = String::new();
+    match file.read_to_string(&mut source) {
+        Err(why) => panic!("couldn't read {}: {}", display, why),
+        Ok(_) => print!("{} contains:\n{}", display, source),
+    }
+
+    let mut lexer = Lexer::new(source.as_str());
 
     let mut token = match lexer.get_token() {
-        None => panic!("Token not found"),
+        None => panic!("token not found"),
         Some(t) => t,
     };
 
     while token.kind != TokenType::Eof {
-        println!("{:?}", token.kind);
+        println!("{:?} - {}", token.kind, token.text);
         token = match lexer.get_token() {
-            None => panic!("Token not found"),
+            None => panic!("token not found"),
             Some(t) => t,
         };
     }
