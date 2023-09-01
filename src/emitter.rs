680f0c14fac2155ced9139e9c789310f3bb036da
Hasan <mr.k779@outlook.com>
2023-09-01 19:07:39 +0600

    Added emitter

--- src/emitter.rs
+++ src/emitter.rs
@@ -1,3 +1,6 @@
+use std::fs::File;
+use std::io::Write;
+
 #[derive(Debug)]
 pub struct Emitter {
     pub full_path: String,
@@ -12,5 +15,23 @@ impl Emitter {
             header: "".to_owned(),
             code: "".to_owned(),
         }
+    }
+
+    pub fn emit(&mut self, code: &str) {
+        self.code = format!("{}{}", self.code, code)
+    }
+
+    pub fn emit_line(&mut self, code: &str) {
+        self.code = format!("{}{}\n", self.code, code)
+    }
+
+    pub fn header_line(&mut self, code: &str) {
+        self.header = format!("{}{}\n", self.header, code)
+    }
+
+    pub fn write_file(&self) {
+        let mut output_file = File::create(&self.full_path).unwrap();
+        output_file.write_all(self.header.as_bytes()).unwrap();
+        output_file.write_all(self.code.as_bytes()).unwrap();
     }
 }
